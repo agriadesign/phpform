6f473968b7e0efe9ea1957a6798864b292bb1de8
phpro <phpform@phpro.hu>
2010-06-03 10:53:17 +0200

    removed version information from the file

--- javascript/config.autocomplete.php
+++ javascript/config.autocomplete.php
@@ -1,9 +1,5 @@
 <?php
 
-/******************************/
-/* version 0.3.0 @ 2010.04.15 */
-/******************************/
-
 define('HOST', 'localhost');
 define('USERNAME', 'phpform');
 define('PASSWD', 'phpform');
